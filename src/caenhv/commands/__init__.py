fc6f273a6d663f8cc8ff305c5c8100d4f10f28c1
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-06-18 20:39:19 +0200

    parsing will throw exception if response is not understood

--- src/caenhv/commands/__init__.py
+++ src/caenhv/commands/__init__.py
@@ -6,33 +6,38 @@ from .module import _get_set_module_command, _get_mon_module_command
 import logging
 
 
-def _parse_response(response: bytes, bd: int | None = None) -> tuple[bool, str]:
+def _parse_response(response: bytes, bd: int | None = None) -> str:
+    """Parse the response from a device.
+
+    Args:
+        response (bytes): The response received from the device.
+        bd (int | None, optional): The expected bd value. Defaults to None.
+
+    Returns:
+        str: The parsed value extracted from the response.
+
+    Raises:
+        ValueError: If the response is invalid, cannot be decoded, or does not match the expected pattern.
+    """
     logger = logging.getLogger(__name__)
     logger.debug(f"Response: {response}")
 
     try:
         response: str = response.decode("utf-8").strip()
     except UnicodeDecodeError:
-        message = f"Error decoding response: '{response}'"
-        logger.error(message)
-        return False, message
+        raise ValueError(f"Invalid response: {response}")
 
     regex = re.compile(r"^#BD:(\d\d),CMD:OK,VAL:(.+)$")
     match = regex.match(response)
     if match is None:
-        message = f"Invalid response: '{response}'"
-        logger.error(message)
-        return False, message
+        raise ValueError(f"Invalid response: '{response}'. Could not match regex")
     bd_from_response = int(match.group(1))
     if bd is not None and bd != bd_from_response:
         message = (
             f"Invalid response: '{response}'. Mismatched bd: {bd_from_response} != {bd}"
         )
-        logger.error(message)
-        return (
-            False,
-            message,
-        )
+        raise ValueError(message)
+
     value: str = match.group(2)
     logger.debug(f"Response result: {response} -> {value}")
-    return True, value
+    return value
--- src/caenhv/devices/channel.py
+++ src/caenhv/devices/channel.py
@@ -37,78 +37,67 @@ class Channel:
     @property
     def vset(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "VSET"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def vmin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "VMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def vmax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "VMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def vdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "VDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def vmon(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "VMON"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def iset(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "ISET"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def imin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "IMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def imax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "IMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def isdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "ISDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def imon(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "IMON"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def imrange(self) -> bool:
         self.write(_get_mon_channel_command(self._bd, self._channel, "IMRANGE"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         if response not in ["HIGH", "LOW"]:
             raise ValueError(f"Unexpected response {response}")
         return response == "HIGH"
@@ -116,189 +105,170 @@ class Channel:
     @property
     def imdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "IMDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def maxv(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "MAXV"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def mvmin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "MVMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def mvmax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "MVMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def mvdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "MVDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def rup(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RUP"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rupmin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RUPMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rupmax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RUPMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rupdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RUPDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def rdw(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RDW"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rdwmin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RDWMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rdwmax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RDWMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def rdwdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "RDWDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def trip(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "TRIP"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def tripmin(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "TRIPMIN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def tripmax(self) -> float:
         self.write(_get_mon_channel_command(self._bd, self._channel, "TRIPMAX"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return float(response)
 
     @property
     def tripdec(self) -> int:
         self.write(_get_mon_channel_command(self._bd, self._channel, "TRIPDEC"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return int(response)
 
     @property
     def pdwn(self) -> str:
         self.write(_get_mon_channel_command(self._bd, self._channel, "PDWN"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return str(response)
 
     @property
     def pol(self) -> str:
         self.write(_get_mon_channel_command(self._bd, self._channel, "POL"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
         return str(response)
 
     @property
     def stat(self) -> str:
-        # TODO: parse status
         self.write(_get_mon_channel_command(self._bd, self._channel, "STAT"))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
-        # TODO: handle error code
+        response = _parse_response(self._serial.readline(), bd=self._bd)
+        # TODO: parse status
         return str(response)
 
     # Setters
     @vset.setter
     def vset(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "VSET", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
+
 
     @iset.setter
     def iset(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "ISET", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @maxv.setter
     def maxv(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "MAXV", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @rup.setter
     def rup(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "RUP", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @rdw.setter
     def rdw(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "RDW", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @trip.setter
     def trip(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "TRIP", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @pdwn.setter
     def pdwn(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "PDWN", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     @imrange.setter
     def imrange(self, value: float) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "IMRANGE", value))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     def on(self) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "ON", None))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
 
     def off(self) -> None:
         self.write(_get_set_channel_command(self._bd, self._channel, "OFF", None))
-        code, response = _parse_response(self._serial.readline(), bd=self._bd)
+        response = _parse_response(self._serial.readline(), bd=self._bd)
