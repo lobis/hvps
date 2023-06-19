015bc8673e9511bded6009dfc017ef14720382f3
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-06-19 18:46:19 +0200

    nodejs bindings find the pyhton package

--- bindings/nodejs/cli.js
+++ bindings/nodejs/cli.js
@@ -1,4 +1,23 @@
-const {run} = require('./index');
+const { ExecutionContext } = require('./index');
+const yargs = require('yargs');
 
-// Call the run function
-console.log("Hello from cli.js")
+const cli = yargs
+    .scriptName('hvps')
+    .usage('$0 <cmd> [args]')
+    .option('python', {
+        description: 'Specify the Python path to run HVPS.',
+        alias: 'p',
+        type: 'string',
+        default: 'python', // Set a default string value
+    })
+    .help()
+    .alias('h', 'help')
+    .argv;
+
+// check python is available
+const context = new ExecutionContext(cli.python);
+context.print();
+
+// Get the remaining arguments as a string
+const remainingArgs = cli._.join(' ');
+console.log('Remaining arguments:', remainingArgs);
--- bindings/nodejs/index.js
+++ bindings/nodejs/index.js
@@ -1,46 +1,62 @@
-const {PythonShell} = require('python-shell');
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 
-function getPythonVersion() {
-    const command = 'python --version';
-    try {
-        return execSync(command).toString().trim();
-    } catch (err) {
-        console.error(`Error executing command (${command}): ${err}`);
-        throw err;
+
+class ExecutionContext {
+    constructor(pythonPath) {
+        this.python = pythonPath;
+        // check if python path is correct and use absolute path
+        this.python = this.getPythonPath();
+
+        // check if the hvps package is available
+        this.getPackageVersion();
+    }
+
+    getPythonVersion() {
+        const command = `${this.python} -c "import sys; print(sys.version)"`;
+        try {
+            return execSync(command, { encoding: 'utf-8' }).trim();
+        } catch (err) {
+            console.error(`Error executing command (${command}): ${err}`);
+            throw err;
+        }
+    }
+
+    getPythonPath() {
+        const command = `${this.python} -c "import sys; print(sys.executable)"`;
+        try {
+            return execSync(command, { encoding: 'utf-8' }).trim();
+        } catch (err) {
+            console.error(`Error executing command (${command}): ${err}`);
+            throw err;
+        }
+    }
+
+    getPackageVersion() {
+        const command = `${this.python} -m hvps --version`;
+        try {
+            return execSync(command, { encoding: 'utf-8' }).trim();
+        } catch (err) {
+            throw new Error(`Error executing command (${command}). Please make sure the package is installed.`);
+        }
+    }
+
+    print() {
+        console.log(`Python version: ${this.getPythonVersion()}`);
+        console.log(`Python path: ${this.getPythonPath()}`);
+        console.log(`Package version: ${this.getPackageVersion()}`);
+    }
+
+    // run method should call the python script with the arguments
+    run(args) {
+        const command = `${this.python} -m hvps ${args}`;
+        try {
+            return execSync(command, { encoding: 'utf-8' }).trim();
+        } catch (err) {
+            throw new Error(`Error executing command (${command}): ${err}`);
+        }
     }
 }
 
-function getPythonPath() {
-    const command = 'python -c "import sys; print(sys.executable)"';
-    try {
-        return execSync(command).toString().trim();
-    } catch (err) {
-        console.error(`Error executing command (${command}): ${err}`);
-        throw err;
-    }
-}
-
-function getPackageVersion() {
-    const command = 'python -m hvps --version';
-    try {
-        return execSync(command).toString().trim();
-    } catch (err) {
-        throw new Error(`Error executing command (${command}). Please make sure the package is installed.`);
-    }
-}
-
-
-function run(args, callback) {
-    const pythonVersion = getPythonVersion();
-    console.log('Python version:', pythonVersion);
-
-    const pythonPath = getPythonPath();
-    console.log('Python path:', pythonPath);
-
-    // print package version
-    const packageVersion = getPackageVersion();
-    console.log('Package version:', packageVersion);
-}
-
-module.exports = run;
+module.exports = {
+    ExecutionContext,
+};
