e1ed627730b2865b887db6016b790cc757cc0f9f
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-06-21 13:12:49 +0200

    parameter parsing and test

--- src/hvps/__main__.py
+++ src/hvps/__main__.py
@@ -50,27 +50,54 @@ def main():
     # validate parameters
     parameter = None
     value = None
+    monitor_mode = False
     if args.brand == "caen":
         parameter = str(args.parameter[0]).upper()
         value = args.value
         channel = args.channel
         if channel is None:
             # parameter is at module level
-            valid_parameters = (
-                list(_mon_module_parameters.keys())
-                if value is None
-                else list(_set_module_parameters.keys())
-            )
+            monitor_parameters = list(_mon_module_parameters.keys())
+            set_parameters = list(_set_module_parameters.keys())
+            monitor_mode = parameter in monitor_parameters
+            if not monitor_mode and parameter not in set_parameters:
+                raise Exception(
+                    f"Parameter '{parameter}' not a valid monitor parameter: {monitor_parameters} or set parameter: {set_parameters}"
+                )
+
         else:
             # parameter is at channel level
-            valid_parameters = (
-                list(_mon_channel_parameters.keys())
-                if value is None
-                else list(_set_channel_parameters.keys())
-            )
+            monitor_parameters = list(_mon_channel_parameters.keys())
+            set_parameters = list(_set_channel_parameters.keys())
 
-        if parameter not in valid_parameters:
-            raise Exception(f"Parameter {parameter} not in {valid_parameters}")
+            if parameter in monitor_parameters and set_parameters:
+                raise Exception(
+                    f"Parameter '{parameter}' is both a monitor and set parameter"
+                )
+
+            monitor_mode = parameter in monitor_parameters
+            if not monitor_mode and parameter not in set_parameters:
+                raise Exception(
+                    f"Channel parameter '{parameter}' not a valid monitor parameter: {monitor_parameters} or set parameter: {set_parameters}"
+                )
+
+            if monitor_mode and value is not None:
+                raise Exception(
+                    f"Channel parameter {parameter} does not accept a value"
+                )
+
+            if not monitor_mode:
+                # set mode
+                if parameter in ["ON", "OFF"]:
+                    if value is not None:
+                        raise Exception(
+                            f"Channel parameter {parameter} does not accept a value"
+                        )
+                else:
+                    if value is None:
+                        raise Exception(
+                            f"Channel parameter {parameter} requires a value"
+                        )
 
     elif args.brand == "iseg":
         raise NotImplementedError("iseg not implemented yet")
@@ -83,21 +110,34 @@ def main():
     if args.brand == "caen":
         module = hvps.module(args.module)
         channel = module.channel(args.channel) if args.channel is not None else None
-        if value is None:
+        if monitor_mode:
+            # monitor parameter
             if not channel:
+                # module level
                 result = getattr(module, parameter)
             else:
+                # channel level
                 result = getattr(channel, parameter)
 
             print(f"{parameter}: {result}")
 
         else:
+            # set parameter
             if not channel:
+                # module level
                 setattr(module, parameter, value)
             else:
-                setattr(channel, parameter, value)
-
-            print(f"{parameter} set to {value}")
+                # channel level
+                if parameter in ["ON", "OFF"]:
+                    if parameter == "ON":
+                        channel.on()
+                        print(f"Channel {channel} turned on")
+                    else:
+                        channel.off()
+                        print(f"Channel {channel} turned off")
+                else:
+                    setattr(channel, parameter, value)
+                    print(f"{parameter} set to {value}")
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,60 @@
+import os
+import subprocess
+import sys
 import pytest
 
-from hvps import HVPS
+
+def run_main_with_arguments(arguments: list) -> tuple:
+    main_file_path = os.path.join(
+        os.path.dirname(__file__), "..", "src/hvps", "__main__.py"
+    )
+    main_file_path = os.path.abspath(main_file_path)
+
+    print(f"__main__.py file path: {main_file_path}")
+
+    process = subprocess.Popen(
+        [sys.executable, main_file_path] + arguments,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    stdout, stderr = process.communicate()
+    exit_code = process.returncode
+
+    return stdout.decode(), stderr.decode(), exit_code
 
 
-@pytest.mark.skip(reason="Needs implementation")
-def test_caen_no_connection():
-    # TODO: implement cli app testing without needing to install (do not just test "python -m hvps")
-    ...
+def test_cli_channel_set():
+    for parameter in [
+        "VSET",
+        "ISET",
+        "MAXV",
+        "RUP",
+        "RDW",
+        "TRIP",
+        "PDWN",
+        "IMRANGE",
+        "ON",
+    ]:
+        arguments = [
+            "--port",
+            "/dev/ttyUSB0",
+            "--baud",
+            "9600",
+            "--module",
+            "1",
+            "--channel",
+            "2",
+            "--test",
+            "caen",
+            f"{parameter}",
+            "100",
+        ]
+
+        stdout, stderr, exit_code = run_main_with_arguments(arguments)
+
+        print(f"arguments: {arguments}")
+        print(f"stdout: {stdout}")
+        print(f"stderr: {stderr}")
+        print(f"exit_code: {exit_code}")
+
+        assert exit_code == 0, f"exit_code: {exit_code} for arguments: {arguments}"
