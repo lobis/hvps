2a33e93a260e8c80c3d902a6f80a28512888e5c7
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-06-22 10:10:36 +0200

    test for channel mon commands cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,6 +23,68 @@ def run_main_with_arguments(arguments: list) -> tuple:
     return stdout.decode(), stderr.decode(), exit_code
 
 
+def test_cli_channel_mon():
+    for parameter in [
+        "VSET",
+        "VMIN",
+        "VMAX",
+        "VDEC",
+        "VMON",
+        "ISET",
+        "IMIN",
+        "IMAX",
+        "ISDEC",
+        "IMON",
+        "IMRANGE",
+        "IMDEC",
+        "MAXV",
+        "MVMIN",
+        "MVMAX",
+        "MVDEC",
+        "RUP",
+        "RUPMIN",
+        "RUPMAX",
+        "RUPDEC",
+        "RDW",
+        "RDWMIN",
+        "RDWMAX",
+        "RDWDEC",
+        "TRIP",
+        "TRIPMIN",
+        "TRIPMAX",
+        "TRIPDEC",
+        "PDWN",
+        "POL",
+        "STAT",
+        "INVALID_PARAMETER",
+    ]:
+        arguments = [
+            "--port",
+            "/dev/ttyUSB0",
+            "--baud",
+            "9600",
+            "--module",
+            "1",
+            "--channel",
+            "2",
+            "--test",
+            "caen",
+            f"{parameter}",
+        ]
+
+        stdout, stderr, exit_code = run_main_with_arguments(arguments)
+
+        print(f"arguments: {arguments}")
+        print(f"stdout: {stdout}")
+        print(f"stderr: {stderr}")
+        print(f"exit_code: {exit_code}")
+
+        exit_code_expected = 0 if parameter not in ["INVALID_PARAMETER"] else 1
+        assert (
+            exit_code == exit_code_expected
+        ), f"exit_code: {exit_code} for arguments: {arguments}"
+
+
 def test_cli_channel_set():
     for parameter in [
         "VSET",
