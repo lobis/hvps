aa6f74439bbebba0886f2316aef0cdcc6473d633
AlonsoDRDLV <alonsodrdlv@gmail.com>
2023-08-05 10:49:10 +0200

    node implement

--- bindings/nodejs/nodered/hvps-nodered.js
+++ bindings/nodejs/nodered/hvps-nodered.js
@@ -2,14 +2,55 @@ hvps = require('hvps')
 
 module.exports = function (RED) {
     function HVPS(config) {
+        this.ports = config.ports
+        this.port = config.port
+        this.baudrate = config.baudrate
+        this.hvps = config.hvps
+        this.module = config.module
+        this.channel_mode = config.channel_mode
+        this.channel = config.channel
+        this.test = config.test
+        this.command = config.command
+
         RED.nodes.createNode(this, config);
         const node = this;
         context = new hvps.ExecutionContext("/usr/bin/python");
+
         node.on('input', function (msg) {
-            msg.payload = context.run(msg.payload);
+
+            // Command builduing
+            if (config.ports){
+                command = "--ports"
+            }
+            else {
+                command = "--port " + config.port + " --baud " + config.baudrate
+                if (config.channel){
+                    command = command + " --channel " + config.channel
+                }
+                if (config.test){
+                    command = command + " --test "
+                }
+                command = command + config.hvps + " "
+                if (config.hvps == "caen"){
+                    command = command + "--module " + config.module
+                }
+                command = command + " " + config.command
+            }
+
+            this.warn(command)
+            msg.payload = context.run(command);
             node.send(msg);
         });
+
     }
 
-    RED.nodes.registerType("hvps", HVPS);
+    RED.nodes.registerType("hvps", HVPS, {
+        // Define the node properties and defaults here
+        settings: {
+            sampleNodeColour: {
+                value: "red",
+                exportable: true
+            }
+        }
+    });
 }
